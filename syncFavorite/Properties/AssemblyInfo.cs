010abd8f8cf999b878bee67b1f836faf7771c903
Ph43n0m <bks42@gmx.net>
2024-12-16 16:03:10 +0100

    add multi target option in ini

--- syncFavorite/Properties/AssemblyInfo.cs
+++ syncFavorite/Properties/AssemblyInfo.cs
@@ -28,5 +28,5 @@ using System.Runtime.InteropServices;
 //      Buildnummer
 //      Revision
 //
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
